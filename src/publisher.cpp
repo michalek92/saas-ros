6c4360f1b565a92ac7acf4d2c575aea46af1acc3
SAAS <SAAS@SAAS.com>
2016-03-07 09:32:33 +0100

    Publish something

--- src/publisher.cpp
+++ src/publisher.cpp
@@ -1,14 +1,19 @@
 #include <ros/ros.h>
+#include <std_msgs/String.h>
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "publisher");
     ros::NodeHandle nh;
     ros::Rate rate(2); // in Hz
+    ros::Publisher ourPub = nh.advertise<std_msgs::String>("chatter", 10);
 
     while(nh.ok())
     {
         ROS_INFO("I'm ok");
+        std_msgs::String msg;
+        msg.data = "ajaja";
+        ourPub.publish(msg);
         rate.sleep();
     }
 
